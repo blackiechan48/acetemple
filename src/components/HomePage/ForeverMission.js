c317293b9d5ca1ba5000d840fa97851c2d0d034f
Olumide Olayeni <oluyeni@gmail.com>
2024-10-29 20:58:23 +0000

    “updates”

--- src/components/HomePage/ForeverMission.js
+++ src/components/HomePage/ForeverMission.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styled, { css } from 'styled-components';
+import styled, { } from 'styled-components';
 
 // Responsive container with flex adjustments for mobile
 const Container = styled.div`
